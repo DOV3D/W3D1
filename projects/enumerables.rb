ea14b6607b3aff70fb0165f8b1185dcd3920b49a
efren <efrenbahen@gmail.com>
2021-08-23 16:09:29 -0500

    enurables methods

--- projects/enumerables.rb
+++ projects/enumerables.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class Array
 
     def my_each(&prc)
@@ -16,12 +17,89 @@ class Array
         end
         return arr
     end
-    
+
+    def my_reject(&prc)       
+        arr =[]
+        self.my_each { |ele| arr << ele unless prc.call(ele)}
+        arr
+    end
+
+    def my_any?(&prc)
+        self.my_each { |ele| return true if prc.call(ele) }
+        false
+    end
+
+    def my_all?(&prc)
+        self.my_each { |ele| return false unless prc.call(ele)}
+        true
+    end
+
+    def my_flatten 
+        arr = []
+        self.each do |ele|
+            if ele.is_a?(Array)
+                arr += ele.my_flatten
+            else
+                arr << ele
+            end 
+        end
+        arr
+    end
+
+    def my_zip(*arrays)
+        zip = []
+
+        (0...self.length).each do |i|
+            subZip = [self[i]]
+            arrays.my_each do |array|
+                subZip << array[i]
+            end
+            zip << subZip
+        end
+        zip
+    end
+
+    def my_rotate(num=1)
+        i = num % self.length
+        
+        i.times {self.push(self.shift) }
+        self
+        
+    end
+   
 end
+
+a = [ "a", "b", "c", "d" ]
+p a.my_rotate         #=> ["b", "c", "d", "a"]
+p a.my_rotate(2)      #=> ["c", "d", "a", "b"]
+p a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
+p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
+
+# c = [10, 11, 12]
+# d = [13, 14, 15]
+# p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
 
 #p [1,2,3].my_each {|el| p el}
 
 
-a = [1, 2, 3]
-p a.my_select { |num| num > 1 } # => [2, 3]
-p a.my_select { |num| num == 4 } # => []
+# a = [1, 2, 3]
+# p a.my_select { |num| num > 1 } # => [2, 3]
+# p a.my_select { |num| num == 4 } # => []
+
+# a = [1, 2, 3]
+# p a.my_reject { |num| num > 1 } # => [1]
+# p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+
+# a = [1, 2, 3]
+# p a.my_any? { |num| num > 1 } # => true
+# p a.my_any? { |num| num == 4 } # => false
+# p a.my_all? { |num| num > 1 } # => false
+# p a.my_all? { |num| num < 4 } # => true
+
+# p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+# a = [ 4, 5, 6 ]
+# b = [ 7, 8, 9 ]
+# p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+# p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+# p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
