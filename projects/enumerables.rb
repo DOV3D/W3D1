9fca5d8ef72aceff1e6dd2a45587d08940e3b807
DOV3D <davidovelazquez3@gmail.com>
2021-08-23 14:17:52 -0400

    First commit

---
+++ projects/enumerables.rb
@@ -0,0 +1,13 @@
+class Array
+
+    def my_each(&prc)
+        i = 0
+        while i < self.length
+            prc.call(self[i])
+            i += 1
+        end
+        self
+    end
+end
+
+p [1,2,3].my_each {|el| p el}
